2b0a3df5d8ae2d2ece3d7a96b8412dd7493d9fdb
BuildTools <unconfigured@null.spigotmc.org>
2022-04-24 23:07:00 -0500

    variable fix

---
+++ src/main/java/org/amusedd/codeblocks/blocks/executables/methods/CodeBlockMethod.java
@@ -0,0 +1,20 @@
+package org.amusedd.codeblocks.blocks.executables.methods;
+
+import org.bukkit.Material;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CodeBlockMethod {
+    String id();
+    String viewName();
+    Material viewMaterial() default Material.STONE;
+    String[] description() default {};
+    String category() default "miscellaneous";
+}
---
+++ src/main/java/org/amusedd/codeblocks/blocks/executables/methods/Param.java
@@ -0,0 +1,17 @@
+package org.amusedd.codeblocks.blocks.executables.methods;
+
+import org.bukkit.Material;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Param {
+    String viewName();
+    Material viewMaterial() default Material.STONE;
+    String[] description() default {};
+    boolean required() default true;
+}
